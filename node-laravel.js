bc2e3e565a85e90213cd5b3fb5898913adda46ea
Richard Remer <rich.remer@zingle.me>
2017-08-08 13:15:38 -0700

    export encrypt/decrypt

--- node-laravel.js
+++ node-laravel.js
@@ -1,5 +1,9 @@
 const laravel = require("./lib/laravel");
 const deserialize = require("./lib/deserialize");
+const decrypt = require("./lib/decrypt");
+const encrypt = require("./lib/encrypt");
 
 module.exports = laravel;
 module.exports.deserialize = deserialize;
+module.exports.decrypt = decrypt;
+module.exports.encrypt = encrypt;
