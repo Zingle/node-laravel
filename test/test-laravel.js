2aaebbc132f9f25302a8ed06a3d324efdaf6d992
Richard Remer <rich.remer@zingle.me>
2017-08-08 13:14:10 -0700

    fix error decrypting final chunk and add tests

--- test/test-laravel.js
+++ test/test-laravel.js
@@ -1,6 +1,12 @@
 const expect = require("expect.js");
 const deserialize = require("../lib/deserialize");
+const decrypt = require("../lib/decrypt");
 const keys = Object.keys;
+
+const key = "12345678911234567892123456789312";
+const iv = "1234567891123456";
+const plain = "foo";
+const cipher = "ff1b56874bd6c3c02f340fd45e9182ee";
 
 // generated using serialize.php script in this directory:
 //  echo '{
@@ -13,6 +19,26 @@ const keys = Object.keys;
 //      "object": {"apple": "banana"}
 //  }' | php serialize.php
 const phpdata = 'O:8:"stdClass":7:{s:4:"true";b:1;s:5:"false";b:0;s:7:"integer";i:23;s:5:"float";d:23.129999999999999;s:6:"string";s:3:"foo";s:5:"array";a:2:{i:0;s:5:"apple";i:1;s:6:"banana";}s:6:"object";O:8:"stdClass":1:{s:5:"apple";s:6:"banana";}}';
+
+describe("decrypt(key, iv, string|Buffer) => string", () => {
+    it("should return decrypted string", () => {
+        expect(decrypt(key, iv, new Buffer(cipher, "hex"))).to.be(plain);
+    });
+
+    it("should accept string or buffer", () => {
+        const strval = decrypt(key, iv, new Buffer(cipher, "hex").toString("binary"));
+        const bufval = decrypt(key, iv, new Buffer(cipher, "hex"));
+        expect(strval).to.be(bufval);
+    });
+
+    it("should error on bad key length", () => {
+        expect(() => decrypt("key", iv, "foo")).to.throwError();
+    });
+
+    it("should error on bad iv length", () => {
+        expect(() => decrypt(key, "iv", "foo")).to.throwError();
+    });
+});
 
 describe("deserialize(string) => *", () => {
     var result;
